e0e75548e00105fda2448c40d2b7d74fe7fcd994
gorokekeron <rafo0860@gmail.com>
2022-05-24 17:28:49 +0900

    모달 애니메이션 추가, 디자인 완료

--- project/javascript.js
+++ project/javascript.js
@@ -1,7 +1,25 @@
 const main_down_btn = document.getElementById("showList")
 const modal_window = document.getElementById("modal");
 
+const modal_closure = document.querySelector(".modal-close span");
+
 
 main_down_btn.addEventListener("click",e=>{
-    modal_window.style.display = "flex"
-})
+    modal_window.style.display = "flex";
+    modal_window.animate([
+        {top:"-100%", easing:"ease-out"},
+        {top:"0%"}
+    ],
+    {duration:350}
+    )
+});
+
+modal_closure.addEventListener("click",e=>{
+    modal_window.animate([
+        {top:"0%", easing:"ease-out"},
+        {top:"-100%"}
+        ],
+        {duration:350}
+    )
+    setTimeout(()=>{modal_window.style.display="none"}, 340);
+});
